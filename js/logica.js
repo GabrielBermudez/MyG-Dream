8353a80118692f38e8536927c2e6a01e3303a353
Gabriel Maximiliano Bermudez <gabrielbermudez0@gmail.com>
2020-08-15 08:25:11 -0300

    Realizo cambios en el archivo js, agrego sidebar, logo de la firma, y mejoro el css

--- js/logica.js
+++ js/logica.js
@@ -1,6 +1,45 @@
 var producto = document.querySelector("#producto");
+var linkProducto = document.querySelector("#linkProducto");
+var linkArtesania = document.querySelector("#linkArtesania");
 
 for (var i = 0; i <5; i++) {
 	producto.insertAdjacentElement('afterend', producto.cloneNode(true));
 
+}
+
+
+linkProducto.onclick = function() {
+	var productosRopa = document.querySelectorAll("#producto");
+	//alert("Acabas de presionar Producto");
+	
+	for(var i = productosRopa.length-1; i > 0; i--){
+		productosRopa[i].remove();
+	}
+
+	producto.querySelector(".photo").src="images/imagen1.png";
+	producto.querySelector("#tituloProducto").innerText="Calza Frizada Talle 1 al 6";
+	producto.querySelector("#precioProducto").innerText="$500";
+
+	for (var i = 0; i <5; i++) {
+	producto.insertAdjacentElement('afterend', producto.cloneNode(true));
+	}
+	
+}
+
+linkArtesania.onclick = function() {
+	//alert("Acabas de presionar Artesania");
+	var productosArtesania = document.querySelectorAll("#producto");
+	for(var i = productosArtesania.length-1; i > 0; i--){
+		productosArtesania[i].remove();
+	}
+
+	producto.querySelector(".photo").src="images/producto2.png";
+	producto.querySelector("#tituloProducto").innerText="Caja de Madera";
+	producto.querySelector("#precioProducto").innerText="$350";
+
+	for (var i = 0; i <5; i++) {
+	producto.insertAdjacentElement('afterend', producto.cloneNode(true));
+
+}
+	
 }
